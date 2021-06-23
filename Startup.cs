4435528cb200cc83381b416a8220891641833569
Arturo Chinchilla <mchinchilla1@estudiantec.cr>
2021-06-23 14:21:48 -0600

    Presentation Model

--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,7 @@ namespace Health_Tec
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup));
             services.AddDbContext<HealthTecDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
